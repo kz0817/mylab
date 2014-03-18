9022c3d7f9ee5e239a85be3daa861d92b08bc380
Kazuhiro Yamato <kazuhiro.yamato@miraclelinux.com>
2014-03-18 12:44:53 +0900

    Add a new directory: sqlite-idx.

---
+++ sqlite-idx/script.sql
@@ -0,0 +1,5 @@
+create table t1(id integer primary key, a integer, b integer);
+insert into t1 values(1, 5, 10);
+insert into t1 values(2, 7, 13);
+insert into t1 values(3, 9, 16);
+insert into t1 values(4, 11, 19);
