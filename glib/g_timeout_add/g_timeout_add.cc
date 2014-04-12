c1787fe9cda85f7ff74379b86f85fca2f81ca93a
Kazuhiro Yamato <kz0817@gmail.com>
2014-04-12 11:03:46 +0900

    Use two timers.

--- glib/g_timeout_add/g_timeout_add.cc
+++ glib/g_timeout_add/g_timeout_add.cc
@@ -8,12 +8,19 @@ static gboolean timeoutCb(gpointer user_data)
 	return G_SOURCE_CONTINUE;
 }
 
+static gboolean timeoutCb2(gpointer user_data)
+{
+	printf("timed out2\n");
+	return G_SOURCE_CONTINUE;
+}
+
 int main(void)
 {
 	GMainLoop *loop = g_main_loop_new(NULL, FALSE);
 	guint interval = 5 * 1000; // ms
 	printf("Start interval timer: interval: %u ms\n", interval);
 	g_timeout_add(interval, timeoutCb, NULL);
+	g_timeout_add(interval*3/5, timeoutCb2, NULL);
 	g_main_loop_run(loop);
 	return EXIT_SUCCESS;
 }
