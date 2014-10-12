5298b2787ed33f9d5aa9b23e98bca7be831cc6cd
Kazuhiro Yamato <kazuhiro.yamato@miraclelinux.com>
2014-10-12 11:03:51 +0900

    [const_ptr] Add patterns.

--- c++/const/const_ptr.cc
+++ c++/const/const_ptr.cc
@@ -1,5 +1,7 @@
 #include <cstdio>
 #include <cstdlib>
+#include <vector>
+using namespace std;
 
 template <typename T>
 class Ptr {
@@ -23,6 +25,13 @@ private:
 
 struct Foo {
 	int x;
+	void foo(void)
+	{
+	}
+
+	void fooConst(void) const
+	{
+	}
 };
 
 int main(void)
@@ -31,16 +40,35 @@ int main(void)
 	foo.x = 5;
 	Ptr<Foo> fooPtr(&foo);
 	fooPtr.get()->x = 10;
+	fooPtr.get()->foo();
+	fooPtr.get()->fooConst();
+	fooPtr.get_const()->foo();
+	fooPtr.get_const()->fooConst();
 
 	Ptr<const Foo> constFooPtr(&foo);
 	printf("constFooPtr->x: %d\n", constFooPtr.get()->x); // read OK.
 	// constFooPtr->x = 20; // Write NG. Build error.
 	// constFooPtr.get_const()->x = 20; // NG. Build error.
 
+	//constFooPtr.get()->foo(); // NG.
+	constFooPtr.get()->fooConst();
+	// constFooPtr.get_const()->foo(); NG
+	constFooPtr.get_const()->fooConst();
+
 	const Ptr<Foo> constPtrFoo(&foo);
 	printf("constPtrFoo->x: %d\n", constFooPtr.get()->x); // read OK.
-	// constPtrFoo.get()->x = 20; // Write NG. Build error
 	constPtrFoo.get_const()->x = 20; // OK.
+
+	//constPtrFoo.get()->foo(); NG.
+	//constPtrFoo.get()->fooConst(); NG.
+	constPtrFoo.get_const()->foo();
+	constPtrFoo.get_const()->fooConst();
+
+	vector<int> intVec;
+	intVec.push_back(5);
+
+	const vector<int> constIntVec;
+	constIntVec.push_back(5);
 
 	return EXIT_SUCCESS;
 }
