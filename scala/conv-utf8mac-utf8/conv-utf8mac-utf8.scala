0708ca0913ff8780cb396ab68135d017f6a73e8c
Kazuhiro Yamato <kazuhiro.yamato@miraclelinux.com>
2015-04-15 16:34:06 +0900

    Add a tool convert string utf8mac to utf8.

---
+++ scala/conv-utf8mac-utf8/conv-utf8mac-utf8.scala
@@ -0,0 +1,49 @@
+import scala.collection.mutable.ListBuffer
+import java.io.InputStreamReader
+
+object ConvUtf8MacUtf8 {
+
+  private val CodeDakuOn    = 0x3099
+  private val CodeHanDakuOn = 0x309a
+  private val dakuonMap = Map(
+    'か' -> 'が', 'き' -> 'ぎ', 'く' -> 'ぐ', 'け' -> 'げ', 'こ' -> 'ご',
+    'さ' -> 'ざ', 'し' -> 'じ', 'す' -> 'ず', 'せ' -> 'ぜ', 'そ' -> 'ぞ',
+    'た' -> 'だ', 'ち' -> 'ぢ', 'つ' -> 'づ', 'て' -> 'で', 'と' -> 'ど',
+    'は' -> 'ば', 'ひ' -> 'び', 'ふ' -> 'ぶ', 'て' -> 'で', 'と' -> 'ど',
+    'カ' -> 'ガ', 'キ' -> 'ギ', 'ク' -> 'グ', 'ケ' -> 'ゲ', 'コ' -> 'ゴ',
+    'サ' -> 'ザ', 'シ' -> 'ジ', 'ス' -> 'ズ', 'セ' -> 'ゼ', 'ソ' -> 'ゾ',
+    'タ' -> 'ダ', 'チ' -> 'ヂ', 'ツ' -> 'ヅ', 'テ' -> 'デ', 'ト' -> 'ド',
+    'は' -> 'ば', 'ひ' -> 'び', 'ふ' -> 'ぶ', 'へ' -> 'べ', 'ほ' -> 'ぼ',
+    'ハ' -> 'バ', 'ヒ' -> 'ビ', 'フ' -> 'ブ', 'ヘ' -> 'ベ', 'ホ' -> 'ボ',
+    'ウ' -> 'ヴ')
+  private val hanDakuonMap = Map(
+    'は' -> 'ぱ', 'ひ' -> 'ぴ', 'ふ' -> 'ぷ', 'へ' -> 'ぺ', 'ほ' -> 'ぽ',
+    'ハ' -> 'パ', 'ヒ' -> 'ピ', 'フ' -> 'プ', 'ヘ' -> 'ペ', 'ホ' -> 'ポ')
+
+  private def printChar(charCode: Char): Char = {
+    print(charCode)
+    return '\0'
+  }
+
+  def main(args: Array[String]): Unit = {
+    val inStream = new InputStreamReader(System.in, "UTF-8")
+    var prevCode = '\0'
+    while (true) {
+      val charCode = inStream.read() match {
+        case -1 => '\0'
+        case code: Int => code.asInstanceOf[Char]
+      }
+      prevCode = charCode match {
+        case CodeDakuOn    => printChar(dakuonMap(prevCode))
+        case CodeHanDakuOn => printChar(hanDakuonMap(prevCode))
+        case _ =>
+          if (prevCode != '\0')
+            printChar(prevCode)
+          charCode
+      }
+      if (charCode == '\0')
+        return
+    }
+  }
+
+}
