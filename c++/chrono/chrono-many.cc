633efb663c56f34b7c8c6d827cb66e1b52363a34
Kazuhiro Yamato <kazuhiro.yamato@miraclelinux.com>
2017-04-20 17:26:10 +0900

    Add a chrono examples.

---
+++ c++/chrono/chrono-many.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <cstdlib>
+#include <chrono>
+
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+	int x = 0;
+	const size_t nloop = atol(argv[1]);
+	chrono::system_clock::time_point t;
+	for (size_t i = 0; i < nloop; i++)
+		t = std::chrono::system_clock::now();
+}
