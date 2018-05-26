2dd73f6373ff2b181a5c6affef858c12edc593c8
Kazuhiro Yamato <kz0817@gmail.com>
2018-05-27 08:39:31 +0900

    [prime-number] Add an implementation by nodejs
    The quick results by each implementation are below.
    C++'s one is slower than node's one.
    I could tune the C++'s implementation.
    
    I used MacBook Pro 2013 late (Core i7 2.6GHz)
    
    [C++]
    $ time c++/pn-c++-O3 -u 1000000
    Warning: unprocessed option: c++/pn-c++-O3
    Upper limit: 1000000
    Count: 78498
    
    real    0m12.707s
    user    0m12.696s
    sys     0m0.009s
    
    [Python]
    $ time python/pn.py -u 1000000
    Upper limit: 1000000
    Count: 78498
    
    real    2m51.282s
    user    2m51.014s
    sys     0m0.165s
    
    [node]
    $ time node node/np.js -u 1000000
    UpperLimit: 1000000
    Count: 78498
    
    real    0m9.724s
    user    0m9.693s
    sys     0m0.024s

---
+++ prime-number/node/np.js
@@ -0,0 +1,60 @@
+const calc = (args) => {
+
+    const primeNumbers = []
+
+    const isPrimeNumber = (n) => {
+        for (let i = 0; i < primeNumbers.length; i++) {
+            const pn = primeNumbers[i]
+            if (pn > n/2)
+                return true
+            if (n % pn  == 0)
+                return false
+        }
+        return true
+    }
+
+    for (let n = 2; n <= args.upperLimit; n++) {
+        if (isPrimeNumber(n))
+            primeNumbers.push(n)
+    }
+
+    console.log("Count: " + primeNumbers.length)
+    if (args.show) {
+        for (let i = 0; i < primeNumbers.length; i++)
+            console.log(primeNumbers[i])
+    }
+}
+
+const parseArgs = (argv) => {
+    const args = {
+        upperLimit: 0,
+        show: false,
+    }
+
+    let nextParser = null;
+    for (let i = 0; i < argv.length; i++) {
+        const arg = argv[i]
+        if (nextParser) {
+            nextParser = nextParser(arg)
+            continue
+        }
+        if (arg == "-u" || arg == "--upper-limit") {
+            nextParser = (arg) => {
+                args.upperLimit = Number(arg)
+                return null
+            }
+        }
+        else if (arg == "-s" || arg ==  "--show") {
+            args.show = true
+        }
+    }
+    return args
+}
+
+const start = () => {
+    const args = parseArgs(process.argv)
+    console.log("UpperLimit: " + args.upperLimit)
+    calc(args)
+}
+
+start()
