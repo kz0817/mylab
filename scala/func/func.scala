8d1886f978d472b24292b7a885505ec00e65c590
Kazuhiro Yamato <kz0817@gmail.com>
2018-08-17 09:31:33 +0000

    Add other function pattern.

--- scala/func/func.scala
+++ scala/func/func.scala
@@ -1,3 +1,8 @@
+class Foo {
+  def func3() = println("func3")
+  def func4(x: Integer) = println(s"func4: ${x}")
+}
+
 object Main {
 
   def func1 = println("func1")
@@ -10,5 +15,13 @@ object Main {
     func2
     // func1() // compile error
     func2()
+
+    val foo = new Foo()
+    foo.func3()
+    // foo.func3{}
+    foo.func4(5)
+    foo.func4{6}
+    //foo.func4 7
+    foo func4 8
   }
 }
