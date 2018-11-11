828ded4dd8ca9137b6db7a4aaa2085ef91274e27
Kazuhiro Yamato <kz0817@gmail.com>
2018-11-11 13:45:07 +0900

    Login/Logout.

--- node/app/app.js
+++ node/app/app.js
@@ -1,24 +1,35 @@
 'use strict'
 const express = require('express')
+const cookieParser = require('cookie-parser')
 const page1 = require('./routes/page1')
 const app = express()
+
+app.use(cookieParser())
 
 app.set('views', './views')
 app.set('view engine', 'pug')
 
 app.get('/', function(req, res, next) {
-  res.render('index')
-})
-
-app.get('/top', function(req, res, next) {
-  res.send('top')
+  const sessionId = req.cookies.sessionId
+  if (!sessionId)
+    res.redirect('login');
+  else
+    res.render('index')
 })
 
 app.get('/login', function(req, res, next) {
     res.render('login')
 })
 
+app.get('/logout', function(req, res, next) {
+  res.clearCookie('sessionId');
+  res.redirect('/')
+})
+
+
 app.post('/login', function(req, res, next) {
+  const expireTime = 3600 * 1000 // milli-sec
+  res.cookie('sessionId', 'test-id')
   res.redirect('/')
 })
 
