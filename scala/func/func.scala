38f09141c99f17aa13adb1e71f7db4cc165923d3
Kazuhiro Yamato <kz0817@gmail.com>
2019-01-04 10:51:49 +0900

    Add a val style function.

--- scala/func/func.scala
+++ scala/func/func.scala
@@ -1,6 +1,8 @@
 class Foo {
   def func3() = println("func3")
   def func4(x: Integer) = println(s"func4: ${x}")
+
+  val func5 = (x: Int) => println(s"func5: ${x}")
 }
 
 object Main {
@@ -8,6 +10,8 @@ object Main {
   def func1 = println("func1")
 
   def func2() = println("func2()")
+
+  def four = (x: Int) => 4 * x
 
   def main(args: Array[String]) {
     println("Hello");
@@ -23,5 +27,9 @@ object Main {
     foo.func4{6}
     //foo.func4 7
     foo func4 8
+
+    foo.func5(10)
+
+    println(four(5))
   }
 }
