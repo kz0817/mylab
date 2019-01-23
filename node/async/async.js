14b48ce9e6ecadf7ab9758dc3f82368d8bcbb557
Kazuhiro Yamato <kz0817@gmail.com>
2019-01-23 23:30:43 +0900

    Add more complicated senario.

--- node/async/async.js
+++ node/async/async.js
@@ -1,20 +1,23 @@
 "use string";
 
-const sleep = (sec) => {
-  console.log("sleep: " + sec);
+const sleep = (sec, name) => {
+  console.log("[" + name + "] sleep: " + sec);
   return new Promise((resolve) => {
-    setTimeout(() => resolve('OK: ' + sec), sec * 1000);
+    setTimeout(() => resolve("[" + name + "] OK: " + sec), sec * 1000);
   });
 }
 
-const func1 = async () => {
-  return await sleep(2);
+const sleep1 = (name) => sleep(1, name);
+
+const func1 = async (name) => {
+  console.log(await sleep1(name));
+  return await sleep(4, name);
 }
 
 console.log("start");
-sleep(3).then((res) => { console.log(res) });
+sleep(3, "A").then((res) => { console.log(res) });
 
 console.log("func1");
-func1().then((res) => { console.log(res) });
+func1("B").then((res) => { console.log(res) });
 
 console.log("end");
