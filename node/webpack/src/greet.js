228b36d629470981a54457e6b2773b19baff9817
Kazuhiro Yamato <kz0817@gmail.com>
2019-03-05 13:21:02 +0900

    Add a webpack example.

---
+++ node/webpack/src/greet.js
@@ -0,0 +1,4 @@
+'use strict';
+
+export const greet = () => 'Hello, world!';
+export const bye = () => 'Sayo nara!';
---
+++ node/webpack/src/index.js
@@ -0,0 +1,6 @@
+'use strict';
+
+import {greet, bye} from './greet';
+
+console.log(greet());
+console.log(bye());
