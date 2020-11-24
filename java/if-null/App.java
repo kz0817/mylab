368bbd945b12cb20cb240acc0b768d65270bd494
YAMATO Kazuhiro <kz0817@gmail.com>
2020-11-24 13:00:19 +0000

    Add a java example to access null at if.

---
+++ java/if-null/App.java
@@ -0,0 +1,10 @@
+public class App {
+    public static void main(String[] args) {
+        Boolean b = null;
+        if (b) {
+            System.out.println("True");
+        } else {
+            System.out.println("False");
+        }
+    }
+}
