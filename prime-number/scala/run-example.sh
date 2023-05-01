75400a6ae2f3736c0f01211e6dc9ce03b4312875
Kazuhiro Yamato <kz0817@gmail.com>
2023-05-01 22:33:36 +0900

    prime-number: scala: Support Int/Long and GraalVM

--- prime-number/scala/run-example.sh
+++ prime-number/scala/run-example.sh
@@ -8,4 +8,7 @@ if [ -f $classpath_file ]; then
   echo source file $classpath_file
   . $classpath_file
 fi
-start 'Scala' "java Main"
+start 'Scala [Int]' "java Main"
+start 'Scala [Long]' "java Main --long"
+start 'Scala (GraalVM) [Int]' "graalvm/bin/java Main"
+start 'Scala (GraalVM) [Long]' "graalvm/bin/java Main --long"
--- prime-number/scala/src/main/scala/pn.scala
+++ prime-number/scala/src/main/scala/pn.scala
@@ -1,6 +1,6 @@
 import scala.util.control.Breaks
 
-class PrimeNumber {
+class PrimeNumberInt {
   val primeNumbers = new scala.collection.mutable.ArrayBuffer[Int]()
   val b = new Breaks
 
@@ -31,12 +31,45 @@ class PrimeNumber {
   }
 }
 
+class PrimeNumberLong {
+  val primeNumbers = new scala.collection.mutable.ArrayBuffer[Long]()
+  val b = new Breaks
+
+  def isPrimeNumber(n: Long): Boolean = {
+    var isPrime = true
+    b.breakable {
+      for (pn <- primeNumbers) {
+        if (pn * pn > n) {
+          isPrime = true
+          b.break
+        }
+        if (n % pn == 0) {
+          isPrime = false
+          b.break
+        }
+      }
+    }
+    return isPrime
+  }
+
+  def calc(upperLimit: Int): Unit = {
+    println("upper limit: " + upperLimit)
+    for (n <- 2 to upperLimit) {
+      if (isPrimeNumber(n))
+        primeNumbers += n
+    }
+    println(s"Count: ${primeNumbers.length}")
+  }
+}
+
 class ArgParser(args: Array[String]) {
   var upperLimit = 0;
+  var varTypeLong = false;
   val it = args.iterator
   while (it.hasNext) {
     it.next match {
       case "-u" => upperLimit = it.next.toInt
+      case "--long" => varTypeLong = true
     }
   }
 }
@@ -44,7 +77,12 @@ class ArgParser(args: Array[String]) {
 object Main {
   def main(args: Array[String]) = {
     val parser = new ArgParser(args)
-    val pn = new PrimeNumber()
-    pn.calc(parser.upperLimit)
+    if (parser.varTypeLong) {
+      println("VarType: Long")
+      (new PrimeNumberLong()).calc(parser.upperLimit)
+    } else {
+      println("VarType: Int")
+      (new PrimeNumberInt()).calc(parser.upperLimit)
+    }
   }
 }
