8bec2186cd073fbf2ed3c2fe7dd9d222fd878c4a
Kazuhiro Yamato <kz0817@gmail.com>
2023-05-02 14:25:33 +0900

    prime-number: php: Add an option to enable JIT
    In a system (Debin 12), the additional option is required
    in order to enable JIT.

--- prime-number/php/run-example.sh
+++ prime-number/php/run-example.sh
@@ -2,4 +2,4 @@
 
 . ../run-helper.sh
 
-start 'PHP' 'php -dmemory_limit=256M -dopcache.enable_cli=1 -dopcache.jit_buffer_size=32M np.php'
+start 'PHP' 'php -dopcache.jit=on -dmemory_limit=256M -dopcache.enable_cli=1 -dopcache.jit_buffer_size=32M np.php'
